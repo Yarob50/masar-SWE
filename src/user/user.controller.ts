e27b3fc93d14c2f7b01053d84f75493341799fb6
Yarob Al Mostafa (يعرُب) <yarob.hm@gmail.com>
2024-11-09 16:22:23 +0300

    some changes

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,43 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpException,
+  HttpStatus,
+  Post,
+} from '@nestjs/common';
+import { DataSource } from 'typeorm';
+import { User } from './user.entity';
 
-@Controller('user')
-export class UserController {}
+@Controller('users')
+export class UserController {
+  constructor(private readonly dataSource: DataSource) {}
+  @Post('')
+  async create(@Body() body) {
+    const userRepo = this.dataSource.getRepository(User);
+    const user = new User();
+    const { fullName, age } = body;
+
+    if (!fullName || !age) {
+      throw new HttpException(
+        'Full name and age are required',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    user.fullName = fullName;
+    user.age = age;
+
+    await userRepo.save(user);
+
+    return { message: 'User created successfully', user: user };
+  }
+
+  @Get('')
+  async findAll() {
+    const userRepo = this.dataSource.getRepository(User);
+    const users = await userRepo.find();
+
+    return { users: users };
+  }
+}
